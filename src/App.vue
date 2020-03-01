0f27fa027cecd3346c3d68a184f96dccf3c42880
y-kawamura <tetora.y@gmail.com>
2020-03-01 21:39:58 +0900

    Add nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
   <div id="app">
+    <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="/#/">
+          Reddit Clone
+        </a>
+
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+    </nav>
     <router-view/>
   </div>
 </template>
