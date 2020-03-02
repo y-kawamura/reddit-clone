91bcbf8a8afc796d63dc7a5c17e515d31d3ef15d
y-kawamura <tetora.y@gmail.com>
2020-03-02 21:07:03 +0900

    Only import used firebase sdk

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDZDRkU2YpFKblJ8Yek487z1PakvotIP5k',
