c0982100d236dee27c6aac2b4ca5c44c30382922
y-kawamura <tetora.y@gmail.com>
2020-03-02 09:05:06 +0900

    No need to set timestampsInSnapshots

--- src/db.js
+++ src/db.js
@@ -1,7 +1,5 @@
 import firebase from '@/firebase';
 
 const firestore = firebase.firestore();
-const settings = { timestampsInSnapshots: true };
-firestore.settings(settings);
 
 export default firestore;
