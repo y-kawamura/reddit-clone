99cbb636aab03372338d16ab441db8928d2ce32c
y-kawamura <tetora.y@gmail.com>
2020-03-04 11:01:09 +0900

    Get subreddits from firestore with vuexfire

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,16 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { vuexfireMutations } from 'vuexfire';
 
 import auth from './auth';
+import subreddits from './subreddits';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  mutations: vuexfireMutations,
   modules: {
     auth,
+    subreddits,
   },
 });
--- src/views/Subreddits.vue
+++ src/views/Subreddits.vue
@@ -1,5 +1,18 @@
 <template>
   <section>
     <h1>Subreddits</h1>
+    <pre>{{ subreddits }}</pre>
   </section>
 </template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+
+export default {
+  mounted() {
+    this.init();
+  },
+  computed: mapState('subreddits', ['subreddits']),
+  methods: mapActions('subreddits', ['init']),
+};
+</script>
