c412ad47e81b36171b89231003ea4260d0dfb45a
y-kawamura <tetora.y@gmail.com>
2020-03-04 17:46:11 +0900

    Show sub reddits list as home page

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Subreddits from '../views/Subreddits.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/subreddits',
     name: 'Subreddits',
-    component: () => import('../views/Subreddits.vue'),
+    component: Subreddits,
   },
   {
     path: '/r/:name',
